133a8c706f1d1307d2fd52fbae63ab5d4c70db99
Katie McLernon <katie.mclernon@kainos.com>
2026-06-05 11:58:19 +0100

    confirmation

--- app/routes.js
+++ app/routes.js
@@ -14,9 +14,11 @@ const EXPERIENCE_LESS_THAN_YEAR = 'Less than a year'
 
 
 router.post ('/new/email-address', (req,res) => {
-
+  if (req.query.returnUrl) {
+    res.redirect(req.query.returnUrl)
+  } else {
     res.redirect ('/new/code')
-
+  }
 })
 
 router.post ('/new/code', (req,res) => {
@@ -25,32 +27,46 @@ router.post ('/new/code', (req,res) => {
 
 })
 
-router.post ('/new/name', (req,res) => {
-
-    res.redirect ('/new/experience')
-
+router.post('/new/name', (req,res) => {
+  if (req.query.returnUrl) {
+    res.redirect(req.query.returnUrl)
+  } else {
+    res.redirect('/new/experience')
+  }
 })
 
-router.post('/experience', function (req, res) {
-  const answer = req.session.data['new'] && req.session.data['new']['yearsOfExperience']
+router.post('/new/experience', function (req, res) {
+  console.log(req.query.returnUrl)
+  if (req.query.returnUrl) {
+    res.redirect(req.query.returnUrl)
+  } else {
+    const answer = req.session.data['new'] && req.session.data['new']['yearsOfExperience']
 
-  console.log('Years of experience selected:', answer)
-
-  if (answer === EXPERIENCE_NONE || answer === EXPERIENCE_LESS_THAN_YEAR) {
-    return res.redirect('/new/ineligible')
+    console.log('Years of experience selected:', answer)
+    if (answer === EXPERIENCE_NONE || answer === EXPERIENCE_LESS_THAN_YEAR) {
+      return res.redirect('/new/ineligible')
+    }
+  
+    return res.redirect('/new/expertise')
   }
-
-  return res.redirect('/new/expertise')
 })
 
 router.post ('/new/expertise', (req,res) => {
-
+if (req.query.returnUrl) {
+    res.redirect(req.query.returnUrl)
+  } else {
     res.redirect ('/new/skills')
-
+  }
 })
 
 router.post ('/new/skills', (req,res) => {
 
     res.redirect ('/new/check')
+
+})
+
+router.post ('/new/check', (req,res) => {
+    console.log('Check your answers page data:', req.session.data['new'])
+    res.redirect ('/new/confirmation')
 
 })
